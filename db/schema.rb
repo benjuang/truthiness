80e0806dfacd94b55b1cd74eff1387c33bdeb9e3
Benjamin Juang <ben.juang@gmail.com>
2012-11-04 10:45:01 -0800

    New columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121103211252) do
+ActiveRecord::Schema.define(:version => 20121104183943) do
 
   create_table "articles", :force => true do |t|
     t.integer  "issue_id"
